78a4ca79e13876a046bd21c153cc23341f5fc6a3
OliverE-creator <oliver.e@cismoss.no>
2025-10-14 08:54:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const facts = [
   "I enjoy reading.",
   "I like to bake."
 ];
-background-color:#002366
+
 const factParagraph = document.getElementById("fact");
 const button = document.getElementById("changeButton");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", ("I love listening to music!") => {
   const randomIndex = Math.floor(Math.random() * facts.length);
   factParagraph.textContent = facts[randomIndex];
 });
